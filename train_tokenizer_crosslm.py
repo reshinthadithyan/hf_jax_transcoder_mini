7cf26465d61af4fc1a7ca5f2be5b132de5c70312
reshinthadithyan <reshinth.adith@gmail.com>
2021-07-09 15:32:25 +0530

    Adding Tokenizer Directory

--- train_tokenizer_crosslm.py
+++ train_tokenizer_crosslm.py
@@ -1,7 +1,8 @@
 from tokenizer.unified_tokenizer_util import load_datset_train_tokenization
 from utils.crosslm_data_utils  import CrossLMDataset
 import tokenizers
-dataset = CrossLMDataset()("train",False)
+dataset = CrossLMDataset()("train",False,False)
+print(dataset)
 def train_tok(dataset,save_dir):
     ''' Initialize and train tokenizer '''
     Tokenizer = tokenizers.ByteLevelBPETokenizer()
@@ -12,8 +13,9 @@ def train_tok(dataset,save_dir):
                                 "</s>",
                                 "<unk>",
                                 "<mask>"],
+                min_frequency=10
                 )
-    Tokenizer.train_from_iterator(dataset)
+    Tokenizer.train_from_iterator([dataset["java"],dataset["cs"]],vocab_size=3000)
     Tokenizer.save_model(save_dir)
 if __name__ == "__main__":
-    train_tok(dataset,r"/Users/reshinthadithyan/master/research/code-research/unsup_translation/models/crosslm")
+    train_tok(dataset,r"/Users/reshinthadithyan/master/research/code-research/unsup_translation/tokenizer_dir/crosslm")
