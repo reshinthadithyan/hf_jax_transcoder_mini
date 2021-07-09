786cbae6bdd2306b845e860d5dbd04b072f46778
reshinthadithyan <reshinth.adith@gmail.com>
2021-07-09 17:57:54 +0530

    adding training args for rough test

--- run_train_jax_crosslm.sh
+++ run_train_jax_crosslm.sh
@@ -11,6 +11,6 @@ python3 train_jax_crosslm.py \
     --learning_rate="3e-4" \
     --warmup_steps="1000" \
     --overwrite_output_dir \
-    --num_train_epochs="50" \
+    --num_train_epochs="1" \
     --push_to_hub \
     --push_to_hub_model_id "reshinthadithyan\transcoder-js-cs"
--- train_jax_crosslm.py
+++ train_jax_crosslm.py
@@ -654,11 +654,16 @@ if __name__ == "__main__":
             write_eval_metric(summary_writer, eval_metrics, cur_step)
 
         # save checkpoint after each epoch and push checkpoint to the hub
-        if jax.process_index() == 0:
-            params = jax.device_get(jax.tree_map(lambda x: x[0], state.params))
-            model.save_pretrained(
-                training_args.output_dir,
-                params=params,
-                push_to_hub=training_args.push_to_hub,
-                commit_message=f"Saving weights and logs of epoch {epoch+1}",
-            )
+        #if jax.process_index() == 0:
+    params = jax.device_get(jax.tree_map(lambda x: x[0], state.params))
+    tokenizer.save_pretrained(
+        training_args.output_dir,
+        push_to_hub=training_args.push_to_hub,
+        commit_message=f"Saving weights and logs of epoch {epoch+1}",
+    )
+    model.save_pretrained(
+        training_args.output_dir,
+        params=params,
+        push_to_hub=training_args.push_to_hub,
+        commit_message=f"Saving weights and logs of epoch {epoch+1}",
+    )
