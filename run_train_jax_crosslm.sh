bd766312c19768c267105bc94acb8ad58013b952
reshinthadithyan <reshinth.adith@gmail.com>
2021-07-09 16:30:47 +0530

    adding bs

--- run_train_jax_crosslm.sh
+++ run_train_jax_crosslm.sh
@@ -6,8 +6,8 @@ python3 train_jax_crosslm.py \
     --dataset_name="crosslm" \
     --dataset_config_name="unshuffled_deduplicated_als" \
     --max_seq_length="128" \
-    --per_device_train_batch_size="5" \
-    --per_device_eval_batch_size="5" \
+    --per_device_train_batch_size="32" \
+    --per_device_eval_batch_size="32" \
     --learning_rate="3e-4" \
     --warmup_steps="1000" \
     --overwrite_output_dir \
