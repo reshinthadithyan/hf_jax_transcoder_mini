906e9ef9876873b015e8dbab52205b2bbe81a444
reshinthadithyan <reshinth.adith@gmail.com>
2021-07-09 16:53:37 +0530

    Increasing num_epochs

--- run_train_jax_crosslm.sh
+++ run_train_jax_crosslm.sh
@@ -5,10 +5,10 @@ python3 train_jax_crosslm.py \
     --tokenizer_name="./models/crosslm" \
     --dataset_name="crosslm" \
     --dataset_config_name="unshuffled_deduplicated_als" \
-    --max_seq_length="128" \
-    --per_device_train_batch_size="32" \
-    --per_device_eval_batch_size="32" \
+    --max_seq_length="256" \
+    --per_device_train_batch_size="4" \
+    --per_device_eval_batch_size="4" \
     --learning_rate="3e-4" \
     --warmup_steps="1000" \
     --overwrite_output_dir \
-    --num_train_epochs="5" 
+    --num_train_epochs="50" 
