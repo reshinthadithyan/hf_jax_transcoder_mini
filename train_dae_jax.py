9d63f988e0b33787c48f75ebc5bec18f55c090cc
dhuruvasaditya <tshere97@gmail.com>
2021-07-10 11:22:12 +0530

    noise refine

--- train_dae_jax.py
+++ train_dae_jax.py
@@ -369,72 +369,7 @@ def compute_input_and_target_lengths(inputs_length, noise_density, mean_noise_sp
 #         span_start_indicator[span_starts] = True
 #         span_num = np.cumsum(span_start_indicator)
 #         is_noise = np.equal(span_num % 2, 1)
-
 #         return is_noise[:orig_length]
-class NoiseAdder():
-    def __init__(self,params,tokenizer):
-        self.mask_index = tokenizer.mask_token_id
-        self.pad_index = tokenizer.pad_token_id
-        self.eos_index = tokenizer.sep_token_id
-        self.sos_index = tokenizer.cls_token_id
-        self.params = params
-        # print(self.mask_index,self.pad_index,self.eos_index)
-
-    def get_before_pad(self,x):
-        '''Obtain length till pad'''
-        try:
-            return np.argwhere(x==0)[0][0]
-        except:
-            return len(x)
-
-    def word_mask(self, x, l):
-        """ Randomly mask input words """
-        # define droppable word indices
-        no_mask = int(l*self.params.word_mask)
-        mask_seq = np.random.randint(2,l,no_mask)
-        
-        x2 = deepcopy(x)
-        for i in mask_seq:
-            x2[i] = self.mask_index
-        return x2
-
-    def word_dropout(self, x, l):
-        """ Randomly drop input words """
-        if self.params.word_dropout == 0:
-            return x
-
-        # define droppable word indices
-        no_drops = int(l*self.params.word_dropout)
-        drop_seq = np.random.randint(2,l,no_drops)
-
-        x2 = deepcopy(x)
-        x2 = np.delete(x2,drop_seq,0)
-        x2 = np.concatenate([x2,[self.mask_index]*no_drops],0)
-        return x2
-
-    def word_shuffle(self, x, l):
-        """ Randomly shuffle input words. """
-        if self.params.word_shuffle == 0:
-            return x
-        
-        # Choose a subsequence to shuffle
-        shuffl_seq = np.random.randint(2,l,2) #leave start token, get start and end of shuffl_seq
-        shuffl_seq.sort()
-
-        x2 = deepcopy(x)
-        x2 = np.concatenate([x[:shuffl_seq[0]],np.random.permutation(x[shuffl_seq[0]:shuffl_seq[1]]),x[shuffl_seq[1]:]],axis=0)
-        return x2
-
-    def add_noise(self, words, lengths):
-        """
-        Add noise to the encoder input.
-        """
-        length = self.get_before_pad(words)
-        words = self.word_shuffle(words, length)
-        words = self.word_dropout(words, length)
-        length = self.get_before_pad(words)
-        words = self.word_mask(words, length)
-        return words
 
 
 @flax.struct.dataclass
