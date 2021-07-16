a6ac200e5eabd45b0ccd8b6b4876df506b31c14c
reshinthadithyan <reshinth.adith@gmail.com>
2021-07-16 19:13:40 +0530

    allows replication

--- train_unsup_bt_bart.py
+++ train_unsup_bt_bart.py
@@ -739,10 +739,8 @@ def main():
         translated = model.generate(batch)
         return translated
     def p_forward_translate(batch):
-        for k in batch:
-            print(batch[k].shape)
         p_generate_forward_translation = jax.pmap(generate_forward_translation,"batch")
-        p_params = model.params#jax_utils.replicate(model.params)
+        p_params = jax_utils.replicate(model.params)
         translated = p_generate_forward_translation(p_params,batch["input_ids"])
         translated_decoded = tokenizer.batch_decode(translated.sequences.reshape(-1,translated.sequences.shape[-1]),skip_special_tokens=True)
         translated_encoded = tokenize_special(translated_decoded,batch)#["input_ids"].shape[1])
