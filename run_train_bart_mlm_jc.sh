7d8501b2001b37657749f7fae29e836bc7a990f2
reshinthadithyan <reshinth.adith@gmail.com>
2021-07-16 22:37:40 +0530

    run_mlm_main training script

---
+++ run_train_bart_mlm_jc.sh
@@ -0,0 +1,16 @@
+python3 train_jax_cross_bart.py \
+    --output_dir="~/models/transcoder-encoder-mlm-jc" \
+    --model_type="bart" \
+    --config_name="./models/crossbartmlm" \
+    --tokenizer_name="./models/crossbartmlm" \
+    --dataset_name="crosslm" \
+    --dataset_config_name="unshuffled_deduplicated_als" \
+    --max_seq_length="512" \
+    --per_device_train_batch_size="8" \
+    --per_device_eval_batch_size="8" \
+    --learning_rate="3e-4" \
+    --warmup_steps="1000" \
+    --overwrite_output_dir \
+    --num_train_epochs="100" \
+    --push_to_hub \
+    --push_to_hub_model_id "reshinthadith/transcoder-encoder-mlm-jc"
